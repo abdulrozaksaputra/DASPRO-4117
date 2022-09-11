8e6ca63e7dba665fcaea0f923fdbc7998d1f553f
Abdul Rozak Saputra 14663 <112887461+abdulrozaksaputra@users.noreply.github.com>
2022-09-11 12:04:59 +0700

    Tugas 01
    Abdul Rozak Saputra

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
+    cout << "|Nama  :Abdul Rozak Saputra                    |" << endl;
+    cout << "|NIM   :A11.2022.14663                         |" << endl;
+    cout << "|Asal Sekolah  :SMA IT LUQMAN AL HAKIM SLAWI   |" << endl;
+    cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
+    return 0;
+}
