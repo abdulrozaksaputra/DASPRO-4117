7ffd6719fc5e24dd4454261594c963b3abe0dff4
Abdul Rozak Saputra 14663 <112887461+abdulrozaksaputra@users.noreply.github.com>
2022-09-17 13:24:09 +0700

    Tugas 2
    Abdul Rozak Saputra 
    A11.2022.14663

---
+++ Tugas2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    // integer
+    int x = 20000;
+    cout << "Abdul Rozak Saputra" << x;
+    /* comment
+    */
+    int a = 18;
+    float b = 171.0;
+    char c = 'XXL';
+    string ub = "XXL";
+
+    cout << "Umur Saya:" << a;
+    cout << endl;
+    cout << "Tinggi Saya:" << b;
+    cout << "\nUkuran Baju:" << ub;
+
+
+    //printf("Uang:");
+    //integer
+
+
+    return 0;
+}
